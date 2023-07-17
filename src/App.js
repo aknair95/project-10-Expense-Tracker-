729d99446da2049ad7d975d888254d95ae41b993
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-18 03:58:00 +0530

    new route home component set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/login";
-import SignUp from "./components/signUp";
+import Login from "./components/pages/login/login";
+import SignUp from "./components/pages/signUp/signUp";
+import Home from "./components/pages/home/home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login/>} />
           <Route path="/signUp" element={<SignUp/>} />
+          <Route path="/home" element={<Home/>} />
         </Routes>
       </BrowserRouter>
     </>
