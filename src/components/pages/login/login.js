353b5f062bebd811d1421749a764ae7ac5608cfc
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-18 03:58:36 +0530

    login and signup files moved to new folder

---
+++ src/components/pages/login/login.js
@@ -0,0 +1,61 @@
+import classes from "./login.module.css";
+import { Button,Container,Form,Navbar } from "react-bootstrap";
+import { useContext, useRef,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login=() =>{
+    const emailRef=useRef();
+    const passwordRef=useRef();
+    const navigate=useNavigate();
+
+    const loginHandler= async (e) =>{
+        e.preventDefault();
+        const enteredEmail=emailRef.current.value;
+        const enteredPassword=passwordRef.current.value; 
+
+        try{ 
+            const response= await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC2XPoZUSexSQEMArfcPRTAXop_LGXmjnY",{
+                email: enteredEmail,
+                password: enteredPassword,
+                returnSecureToken: true
+             });
+             localStorage.setItem("token",response.data.idToken);
+             localStorage.setItem("emailId",enteredEmail);
+            } catch(error){
+                alert("!!! Incorrect Email or Password !!!");
+            }
+
+        emailRef.current.value="";
+        passwordRef.current.value="";
+
+        navigate("/home");
+    }
+
+    const createNewAccHandler=() =>{
+        navigate("/signUp");
+    }
+
+    return(
+    <>
+        <Container className={classes.formContainer}>
+            <h3 className="p-2">LOGIN</h3>
+            <Form onSubmit={loginHandler}>
+                <Form.Group className="p-3">                    
+                    <Form.Control type="email" placeholder="Enter Email ID" required size="lg" ref={emailRef}/>
+                </Form.Group>
+                <Form.Group className="p-3">                      
+                    <Form.Control type="password" placeholder="Enter Password" required size="lg" ref={passwordRef} />
+                </Form.Group>
+                <div className={classes.Btns}>
+                    <Button type="submit" size="lg">LOGIN</Button>
+                    <Button variant="link" size="lg">Forgot Password</Button>
+                    <Button variant="link" size="lg" onClick={createNewAccHandler}>Create New Account</Button>
+                </div>
+            </Form>
+        </Container>     
+    </>
+    )
+}
+
+export default Login;
---
+++ src/components/pages/signUp/signUp.js
@@ -0,0 +1,67 @@
+import classes from "./signUp.module.css";
+import { Button,Container,Form,Navbar } from "react-bootstrap";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const SignUp=() =>{
+    const emailRef=useRef();
+    const passwordRef=useRef();
+    const confirmPasswordRef=useRef();
+    const navigate=useNavigate();
+
+    const signUpHandler= async (e) =>{
+        e.preventDefault();
+        const enteredEmail=emailRef.current.value;
+        const enteredPassword=passwordRef.current.value;
+        const confirmPassword=confirmPasswordRef.current.value; 
+
+        if(enteredPassword===confirmPassword){
+            try{ 
+            await axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC2XPoZUSexSQEMArfcPRTAXop_LGXmjnY",{
+                email: enteredEmail,
+                password: enteredPassword,
+                returnSecureToken: true
+             });
+            alert("Signed up successfully"); 
+            navigate("/login");
+            } catch(error){
+                alert("Please enter valid email & password(min length- 6 character)");
+            }
+        } else{
+            alert("!!! Incorrect Password. Please Enter Again !!!");
+        }
+        
+        emailRef.current.value="";
+        passwordRef.current.value="";
+    }
+
+    const loginExistingAccHandler=() =>{
+        navigate("/login");
+    }
+
+    return(
+    <>
+        <Container className={classes.formContainer}>
+            <h3 className="p-2">SIGN UP</h3>
+            <Form onSubmit={signUpHandler}>
+                <Form.Group className="p-3">                    
+                    <Form.Control type="email" placeholder="Enter Email ID" required size="lg" ref={emailRef}/>
+                </Form.Group>
+                <Form.Group className="p-3">                      
+                    <Form.Control type="password" placeholder="Enter Password" minLength={6} required size="lg" ref={passwordRef} />
+                </Form.Group>
+                <Form.Group className="p-3">                      
+                    <Form.Control type="password" placeholder="Confirm Password" minLength={6} required size="lg" ref={confirmPasswordRef} />
+                </Form.Group>
+                <div className={classes.Btns}>
+                    <Button type="submit" size="lg">SIGN UP</Button>
+                    <Button variant="link" size="lg" onClick={loginExistingAccHandler}>Login With Existing Account</Button>
+                </div>
+            </Form>
+        </Container>     
+    </>
+    )
+}
+
+export default SignUp;
