7cf4288a47176818c74ed751f10e4dcead002b92
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-18 21:32:43 +0530

    home component complete now btn added & css update

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,7 +1,17 @@
+import { Button } from "react-bootstrap";
+import classes from "./home.module.css"
 
 const Home=() =>{
     return(
-        <h1>Welcome to Expense Tracker APP</h1>
+        <>
+            <header className={classes.header}>
+                <h2>!!! Welcome to Expense Tracker!!!</h2>
+                <h5>Your Profile Is Incomplete.</h5>
+                <Button variant="link" size="lg">Complete Now</Button>
+            </header>
+            <hr/>
+        </>
+        
     )
 }
 
