ae0b902f79fb417ac82ab5af64fd526d252382b1
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-20 03:30:39 +0530

    conditional rendering to hide complete now btn

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,6 +1,7 @@
 import { Button } from "react-bootstrap";
 import classes from "./home.module.css"
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Home=() =>{
     const navigate=useNavigate();
@@ -8,12 +9,19 @@ const Home=() =>{
         navigate("/updateProfile");
     }
 
+    const [profileUpdated,setProfileUpdated]= useState(false);
+
+    useEffect(() =>{
+        const profileStatus=localStorage.getItem("profileUpdated");
+        setProfileUpdated(profileStatus);
+    },[])
+
     return(
         <>
             <header className={classes.header}>
                 <h2>!!! Welcome to Expense Tracker!!!</h2>
-                <h5>Your Profile Is Incomplete.</h5>
-                <Button variant="link" size="lg" onClick={completeNowBtnHandler}>Complete Now</Button>
+                {!profileUpdated && <h5>Your Profile Is Incomplete.</h5>}
+                {!profileUpdated && <Button variant="link" size="lg" onClick={completeNowBtnHandler}>Complete Now</Button>}
             </header>
             <hr/>
         </>
