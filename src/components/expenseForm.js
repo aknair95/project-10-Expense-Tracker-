c812085bb27bc75e417abfdec36f5726989997c0
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-07-24 02:38:49 +0530

    expense form component created with css to enter expense details

---
+++ src/components/expenseForm.js
@@ -0,0 +1,61 @@
+import classes from "./expenseForm.module.css";
+import { Button,Container,Form } from "react-bootstrap";
+import { useRef } from "react";
+
+const ExpenseForm=(props) =>{
+    const amountRef=useRef();
+    const descriptionRef=useRef();
+    const categoryRef=useRef();
+
+    const addExpenseHandler= async (e) =>{
+        e.preventDefault();
+        const enteredAmount=amountRef.current.value;
+        const enteredDescription=descriptionRef.current.value; 
+        const enteredCategory=categoryRef.current.value; 
+
+        const newExpense={
+            amount: enteredAmount,
+            description: enteredDescription,
+            category: enteredCategory
+        }
+
+        props.addNewExpense(newExpense);
+
+        amountRef.current.value="";
+        descriptionRef.current.value="";
+        categoryRef.current.value="Select Category";
+    }
+
+    return(
+    <>
+        <Container className={classes.formContainer}>
+            <h3 className="p-2">ADD NEW EXPENSE</h3>
+            <Form onSubmit={addExpenseHandler}>
+                <Form.Group className="p-3">                    
+                    <Form.Control type="number" placeholder="Enter Amount" required size="lg" ref={amountRef}/>
+                </Form.Group>
+                <Form.Group className="p-3">                      
+                    <Form.Control type="text" placeholder="Enter Description" required size="lg" ref={descriptionRef} />
+                </Form.Group>
+                <Form.Group className="p-3">
+                    <Form.Select ref={categoryRef} size="lg">
+                        <option>Select Category</option>
+                        <option value="food">FOOD</option>
+                        <option value="grocery">GROCERY</option>
+                        <option value="fuel">FUEL</option>
+                        <option value="travel">TRAVEL</option>
+                        <option value="bills">BILLS</option>
+                        <option value="clothing">CLOTHING</option>
+                        <option value="others">OTHERS</option>
+                    </Form.Select>
+                </Form.Group>
+                <div className={classes.Btn}>
+                    <Button type="submit" size="lg">ADD EXPENSE</Button>
+                </div>
+            </Form>
+        </Container>     
+    </>
+    )
+}
+
+export default ExpenseForm;
