766f7eb589a12102d2c5b4d703823c22af357778
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-08-10 18:37:06 +0530

    updated main reducer for theme reducer

--- src/store/reducerMain.js
+++ src/store/reducerMain.js
@@ -2,9 +2,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authReducer";
 import expensesReducer from "./expensesReducer";
+import themeReducer from "./themeReducer";
 
 const storeReducer= configureStore({
-    reducer: { authentication: authReducer.reducer,expenses: expensesReducer.reducer }
+    reducer: { authentication: authReducer.reducer,expenses: expensesReducer.reducer,theme: themeReducer.reducer }
 });
 
 export default storeReducer;
